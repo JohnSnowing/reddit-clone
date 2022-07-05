9db8e362fc7e7ce0fd0148a9b2287e6bb784b5e5
Jonmari <jonmari@simplecloudology.com>
2022-07-06 01:05:50 +0800

    initial commit

---
+++ src/components/Navbar/SearchInput.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+type SearchInputProps = {};
+
+const SearchInput: React.FC<SearchInputProps> = () => {
+    return <Flex></Flex>;
+};
+export default SearchInput;
