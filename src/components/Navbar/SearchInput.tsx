30fb824b2e0700bb61493b035320848b59550c1d
Jonmari <jonmari@simplecloudology.com>
2022-11-07 03:36:30 +0800

    create navbar search input

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,9 +1,35 @@
-import { Flex } from "@chakra-ui/react";
+import { PhoneIcon, SearchIcon } from "@chakra-ui/icons";
+import {
+    Flex,
+    Input,
+    InputGroup,
+    InputLeftElement,
+    InputRightElement,
+} from "@chakra-ui/react";
 import React from "react";
 
 type SearchInputProps = {};
 
 const SearchInput: React.FC<SearchInputProps> = () => {
-    return <Flex></Flex>;
+    return (
+        <Flex>
+            <InputGroup>
+                <InputLeftElement
+                    pointerEvents="none"
+                    children={<SearchIcon color="gray.300" />}
+                />
+                <Input
+                    placeholder="Search Reddit"
+                    fontSize="10pt"
+                    _placeholder={{ color: "gray.500" }}
+                    _hover={{
+                        bg: "white",
+                        border: "1px solid",
+                        borderColor: "blue.500",
+                    }}
+                />
+            </InputGroup>
+        </Flex>
+    );
 };
 export default SearchInput;
