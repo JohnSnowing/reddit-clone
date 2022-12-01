7d5665c4006cb05fefed0d782fb76fe5246d0169
Jonmari <jonmari@simplecloudology.com>
2022-12-01 21:15:35 +0800

    Create User menu dropdown item

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import AuthModal from "../../Modal/Auth/AuthModal";
 import AuthButton from "./AuthButton";
 import Icons from "./Icons";
+import MenuWrapper from "./ProfileMenu/MenuWrapper";
 
 type RightContentProps = {
     user: User;
@@ -15,6 +16,7 @@ const RightContent: React.FC<RightContentProps> = ({ user }) => {
             <AuthModal />
             <Flex justifyContent="space-between" alignItems="center">
                 {user ? <Icons /> : <AuthButton />}
+                <MenuWrapper />
             </Flex>
         </>
     );
